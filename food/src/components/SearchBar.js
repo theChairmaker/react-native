ce7eb1fdb47f3d18901b976d9f17eb8e201d1b0b
Tomislav Andros <r3r00t3d@gmail.com>
2020-06-26 00:19:56 +0200

    Added food app

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {View, TextInput, StyleSheet, Platform } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Ionicons
+        name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'}
+        style={styles.iconStyle}
+      />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+        onEndEditing={() => onTermSubmit()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: '#E4E7EB',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
