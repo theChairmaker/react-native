88ca69f470bd1065c797b77b8e5943795b179aaf
Tomislav Andros <r3r00t3d@gmail.com>
2020-07-01 10:27:11 +0200

    Created backend for tracking routes

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,14 +3,14 @@ import createDataContext from "./createDataContext";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "delete_blogpost":
-      return state.filter((blogPost) => blogPost.id !== action.payload); 
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blog_post":
       return [
         ...state,
         {
           id: Math.floor(Math.random() * 99999),
-          title: `Blog post #${state.length + 1}`
-        }
+          title: `Blog post #${state.length + 1}`,
+        },
       ];
     case "default":
       return state;
@@ -23,11 +23,11 @@ const addBlogPost = (dispatch) => {
   };
 };
 
-const deleteBlogPost = dispatch => {
+const deleteBlogPost = (dispatch) => {
   return (id) => {
     dispatch({ type: "delete_blogpost", payload: id });
   };
-}
+};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
