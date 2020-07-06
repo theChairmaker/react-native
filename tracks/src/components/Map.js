604f3e1d33a322ad88b5c74ddbfc77e860bf9384
Tomislav Andros <r3r00t3d@gmail.com>
2020-07-06 11:08:46 +0200

    Added geolocation package

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
-  return <Text>This is map.</Text>;
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    points.push({
+      latitude: 45.7989887 + i * 0.001,
+      longitude: 15.9384538 + i * 0.001
+    });
+  };
+  return (
+    <MapView
+      initialRegion={{
+        latitude: 45.7989887,
+        longitude: 15.9384538,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+      style={styles.map}
+    >
+      <Polyline coordinates={points} />
+    </MapView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default Map;
